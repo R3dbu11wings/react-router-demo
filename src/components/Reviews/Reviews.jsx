0f82f567611fa6c3a6fec046e48fef05fdd39452
Zachary Colton tesch <zachctesch_10@hotmail.com>
2025-10-22 22:52:08 -0500

    route added

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,9 +1,20 @@
-function Reviews() {
+import './Reviews.css';
+import { useParams } from 'react-router-dom';
+
+function Reviews({ reviews }) {
+
   return (
-    <div>
-      <p>My reviews will go here!</p>
+    <div className="reviews">
+      <ul className="reviews__list">
+        {reviews && reviews.map((review) => {return (
+  <li key={review.id} className="reviews__item">
+    <Link to={`${review.id}`}>{review.title}</Link>
+  </li>
+);
+        })}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default Reviews;
